74c48d4e779beb78902878c0b6345ba4b5601e6b
yuusheng <contact@yuusheng.cn>
2022-07-31 22:06:44 +0800

    chore: vite

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,15 @@ import { canvasSize } from './globalVar'
 import { initCanvas, drawClock } from './components/canvas/index'
 import { onTime, countDown, timer } from './components/timer/index'
 import palette from './components/canvas/color'
+import './static/index.css'
+import 'uno.css'
 
 // 初始化背景、画布
 let bgColor = palette.blue
 const canvas = <HTMLCanvasElement>document.getElementById('canvas')
 const ctx = initCanvas(canvas, canvasSize.width, canvasSize.height)
 
+console.log('1')
 // 计时部分
 onTime(ctx, drawClock)
 
@@ -16,10 +19,13 @@ let countTime = 25
 const startCount = document.querySelector('#startCount')
 // 输入框
 let countTimeInput = <HTMLInputElement>document.getElementById('countTime')
+countTimeInput.value = '25'
 // 按钮按下开始倒计时25min
 startCount.addEventListener('click', () => {
   if (countTimeInput.value) {
     countTime = +countTimeInput.value
+  } else {
+    countTime = 25
   }
 
   bgColor = palette.cyan
---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import Unocss from 'unocss/vite'
+import presetWind from '@unocss/preset-wind'
+import presetAttributify from '@unocss/preset-attributify'
+import { resolve } from 'path'
+
+// https://vitejs.dev/config/
+const config = defineConfig({
+  base: process.env.BASE_PATH ?? '/',
+
+  plugins: [
+    // vue({ reactivityTransform: true }),
+    Unocss({
+      presets: [presetWind, presetAttributify],
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, '/src'),
+    },
+  },
+})
+
+export default config

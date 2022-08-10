bbd296be5fc2233b2064fdf56dbc0b306206fc2c
yuusheng <contact@yuusheng.cn>
2022-08-10 16:37:50 +0800

    feat: add router

--- src/router.ts
+++ src/router.ts
@@ -12,6 +12,16 @@ const routes = [
     component: () => import('~/pages/Statistics/Index.vue'),
   },
   {
+    path: '/user',
+    name: 'user',
+    component: () => import('~/pages/User/Index.vue'),
+  },
+  {
+    path: '/star',
+    name: 'star',
+    component: () => import('~/pages/Star/Index.vue'),
+  },
+  {
     path: '/settings',
     name: 'settings',
     component: () => import('~/pages/Settings/Index.vue'),
